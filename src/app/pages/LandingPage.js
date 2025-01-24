f17943d61860dbedf1764b1a173a85289a2594d8
aqdhasali <aqdhas@commercializer.global>
2025-01-24 15:47:28 +0530

    +

--- src/app/pages/LandingPage.js
+++ src/app/pages/LandingPage.js
@@ -18,14 +18,14 @@ export default function LandingPage(){
             </div>
 
             <div className="flex flex-col-reverse lg:flex-row lg:justify-between items-center">
-                <div className='flex flex-col gap-5 items-center lg:items-start mt-5'>
+                <div className='flex flex-col gap-5 items-center p-10 lg:items-start mt-5'>
                     <div className='flex flex-col gap-5'>
                         <h1 className={`${roboto.className} text-5xl text-center lg:text-left font-bold`}>
                         Meet MERZ Wallet,<br/>
-                        Sleek. Secure. Smart
+                        Sleek. Secure. Smart.
                         </h1>
 
-                        <h1 className='text-gray-400 text-center lg:text-left'> Minimalist design, maximum functionality.<br/>
+                        <h1 className='text-gray-400 text-lg text-center lg:text-left'> Minimalist design, maximum functionality.<br/>
                         Your everyday essential redefined</h1>
                     </div>
 
